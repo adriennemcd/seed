543435f35d815b4e96132d6dd034e7fb0f849cef
adriennemcd <adrienne.mcd.21@gmail.com>
2017-01-04 19:37:53 -0500

    add browsersync to gulpfile

--- wp-content/themes/seed/gulpfile.js
+++ wp-content/themes/seed/gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     globbing = require('gulp-css-globbing'),
     autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 
 
 // CSS to SASS =====================================================================
@@ -16,12 +18,29 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream())
 });
 gulp.task('watch:sass', function() { gulp.watch('src/sass/**/*.scss', gulp.series('sass')); });
 
 
+// Browsersync ===========================================================
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    files: ['./**/*.php'],
+    proxy: 'http://localhost/seed/',
+  });
+  gulp.task('watch', gulp.parallel('watch:sass', ['sass', reload]));
+  // gulp.watch('./js/*.js', ['js', reload]);
+  // gulp.watch('images/src/*', ['images', reload]);
+});
+
+
 // Putting it altogether! ===========================================================
 
-gulp.task('watch', gulp.parallel('watch:sass'));
+gulp.task('watch', gulp.parallel('watch:sass', 'browser-sync'));
 gulp.task('default', gulp.series('sass', 'watch'));
+
+
+
